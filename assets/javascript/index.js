c4e587fadcdb3d3a99df266be058028981e991c5
Darion Richards <darionrichards@outlook.com>
2021-10-20 12:14:52 +0100

    Cleaned styling of elements with mobile viewports

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,1 +1,60 @@
-console.log('loaded');
+const mockData = {
+    current: {
+        name: "London",
+        temperature: 123.45,
+        wind: 111.22,
+        humidity: 33,
+        uvi: 2.5,
+        date: "(3/30/2021)",
+        iconCode: "04n",
+    },
+    forecast: [{
+            date: "(3/30/2021)",
+            temperature: 123.45,
+            wind: 111.22,
+            humidity: 33,
+            iconCode: "04n",
+        },
+        {
+            date: "(3/30/2021)",
+            temperature: 123.45,
+            wind: 111.22,
+            humidity: 33,
+            iconCode: "04n",
+        },
+        {
+            date: "(3/30/2021)",
+            temperature: 123.45,
+            wind: 111.22,
+            humidity: 33,
+            iconCode: "04n",
+        },
+        {
+            date: "(3/30/2021)",
+            temperature: 123.45,
+            wind: 111.22,
+            humidity: 33,
+            iconCode: "04n",
+        },
+        {
+            date: "(3/30/2021)",
+            temperature: 123.45,
+            wind: 111.22,
+            humidity: 33,
+            iconCode: "04n",
+        },
+    ],
+};
+
+const onLoad = () => {
+    // get from ls
+    // render recent searches
+}
+
+const showCity = () => {
+    console.log('show city')
+}
+
+$('#form-container').on('click', showCity)
+
+$(document).ready(onLoad)
