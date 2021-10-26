6872f6273affe85fc25ff0b8b08cd7c8d2386fc7
Darion Richards <darionrichards@outlook.com>
2021-10-26 14:16:59 +0100

    Created function to get API weather data
    - created function getCurrentDate() to return current date
    - created onLoad() function to render weather information when page is ready and fully loaded
    - added moment.js script to index.html file

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,69 +1,88 @@
-const mainContentWrapper = $('#main-content-wrapper')
+const currentWeatherContainer = $('#current-weather-container');
 
-const mockData = {
-    current: {
-        name: "London",
-        temperature: 123.45,
-        wind: 111.22,
-        humidity: 33,
-        uvi: 2.5,
-        date: "(3/30/2021)",
-        iconCode: "04n",
-    },
-    forecast: [{
-            date: "(3/30/2021)",
-            temperature: 123.45,
-            wind: 111.22,
-            humidity: 33,
-            iconCode: "04n",
+const API_KEY = `7d9d6a42892e49768b347537bd606146`
+
+const getCurrentDate = () => {
+    let date = moment().format('MMMM Do YYYY');
+    return date
+};
+
+const getWeatherData = async(cityName) => {
+    const currentDataUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const currentDataResponse = await fetch(currentDataUrl);
+    const currentData = await currentDataResponse.json();
+    console.log(currentData);
+
+    const lat = currentData.coord.lat;
+    const lon = currentData.coord.lon;
+    const name = currentData.name;
+
+    const forecastDataUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=imperial`;
+
+    const forecastDataResponse = await fetch(forecastDataUrl);
+    const forecastData = await forecastDataResponse.json();
+    console.log(forecastData)
+
+    return {
+        current: {
+            name: name,
+            temperature: forecastData.current.temp,
+            wind: forecastData.current.wind_speed,
+            humidity: forecastData.current.humidity,
+            uvi: forecastData.current.uvi,
+            date: getCurrentDate(),
+            iconCode: forecastData.current.weather[0].icon,
         },
-        {
-            date: "(3/31/2021)",
-            temperature: 123.45,
-            wind: 111.22,
-            humidity: 33,
-            iconCode: "04n",
-        },
-        {
-            date: "(3/32/2021)",
-            temperature: 123.45,
-            wind: 111.22,
-            humidity: 33,
-            iconCode: "04n",
-        },
-        {
-            date: "(3/33/2021)",
-            temperature: 123.45,
-            wind: 111.22,
-            humidity: 33,
-            iconCode: "04n",
-        },
-        {
-            date: "(3/34/2021)",
-            temperature: 123.45,
-            wind: 111.22,
-            humidity: 33,
-            iconCode: "04n",
-        },
-    ],
+        forecast: [{
+                date: "(3/30/2021)",
+                temperature: 123.45,
+                wind: 111.22,
+                humidity: 33,
+                iconCode: "04n",
+            },
+            {
+                date: "(3/31/2021)",
+                temperature: 123.45,
+                wind: 111.22,
+                humidity: 33,
+                iconCode: "04n",
+            },
+            {
+                date: "(3/32/2021)",
+                temperature: 123.45,
+                wind: 111.22,
+                humidity: 33,
+                iconCode: "04n",
+            },
+            {
+                date: "(3/33/2021)",
+                temperature: 123.45,
+                wind: 111.22,
+                humidity: 33,
+                iconCode: "04n",
+            },
+            {
+                date: "(3/34/2021)",
+                temperature: 123.45,
+                wind: 111.22,
+                humidity: 33,
+                iconCode: "04n",
+            },
+        ],
+    }
 };
 
 const renderCurrentWeather = currentWeather => {
-    const currentWeatherContainer = `
-    <section class="container current-weather-container">
-        <h2>${currentWeather.name} | ${currentWeather.date} | <img src="https://openweathermap.org/img/w/${currentWeather.iconCode}.png" /></h2>
+    const currentWeatherCard = `<h2>${currentWeather.name} | ${currentWeather.date} | <img src="https://openweathermap.org/img/w/${currentWeather.iconCode}.png"/></h2>
         <p>Temp: ${currentWeather.temperature} &degF;</p>
         <p>Wind: ${currentWeather.wind}MPH</p>
         <p>Humidity: ${currentWeather.humidity}%</p>
-        <p>UV Index: ${currentWeather.uvi}</p>
-    </section>`;
+        <p>UV Index: ${currentWeather.uvi}</p>`;
 
-    mainContentWrapper.append(currentWeatherContainer);
+    currentWeatherContainer.append(currentWeatherCard);
 };
 
 const renderForecastCardsContainer = () => {
-    const constructForecastContainer = `<section class="daily-forecast-container" id="daily-forecast-container"></section>`;
-    mainContentWrapper.append(constructForecastContainer);
 
     const forecastContainer = $('#daily-forecast-container');
 
@@ -75,6 +94,7 @@ const renderForecastCardsContainer = () => {
 };
 
 const renderForecastCards = forecastData => {
+
     renderForecastCardsContainer();
 
     const constructCard = forecastDay => {
@@ -99,4 +119,10 @@ const renderWeatherCards = weatherData => {
     renderForecastCards(weatherData.forecast)
 };
 
-renderWeatherCards(mockData);
+const onLoad = async() => {
+    const weatherData = await getWeatherData("London")
+    getCurrentDate();
+    renderWeatherCards(weatherData);
+};
+
+$(document).ready(onLoad)
